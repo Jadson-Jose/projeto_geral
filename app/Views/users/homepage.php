942efb6d0fec7d7de148bdb2793a0962474f5785
JadsonJose <jadsonjosesilva@gmail.com>
2020-09-29 17:26:50 -0300

    sinc

--- app/Views/users/homepage.php
+++ app/Views/users/homepage.php
@@ -7,10 +7,21 @@ $s = session();
 
 <?php $this->section('conteudo') ?>
 
-<div>Olá, <?php echo $s->name . '.' ?></div>
+    <div>Olá, <?php echo $s->name . '.' ?></div>
 
-<div>O meu perfil é de: <?php echo $s->profile ?></div>
+    <div>O meu perfil é de : <?php echo $s->profile ?></div>
 
-<a href="<?php echo site_url('users/logout') ?>">Logout</a>
+    <div class="row">
+        <div class="col-4 text-center"><a href="<?php echo site_url('users/op1') ?>" class="btn btn-primary">Operação 1</a></div>
+        <div class="col-4 text-center"><a href="<?php echo site_url('users/op2') ?>" class="btn btn-primary">Operação 2</a></div>
+        
+        <?php if(isset($admin)) : ?>        
+            <div class="col-4 text-center"><a href="<?php echo site_url('users/admin_users') ?>" class="btn btn-primary">Gestão de utilizadores</a></div>
+        <?php endif; ?>    
+    </div>
+
+
+
+    <a href="<?php echo site_url('users/logout') ?>">Logout</a>
 
 <?php $this->endSection() ?>
