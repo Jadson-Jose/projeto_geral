80ee2b5e6b5af365a3b8836f41b82ed60b5ec607
JadsonJose <jadsonjosesilva@gmail.com>
2020-10-17 17:29:05 -0300

    sinc

--- app/Views/users/admin_users.php
+++ app/Views/users/admin_users.php
@@ -18,9 +18,9 @@ $s = session();
                     <th>Nome</th>
                     <th>Email</th>
                     <th>Último login</th>
-                    <th>Profile</th>
-                    <th>Ativo</th>
-                    <th>Eliminado</th>
+                    <th class = "text-center">Profile</th>
+                    <th class = "text-center">Ativo</th>
+                    <th class = "text-center">Eliminado</th>
                 </thead>
 
                 <tbody>
@@ -29,15 +29,37 @@ $s = session();
                             <!-- editar e eliminar -->
                             <td>
                                 <a href="" class= "btn btn-primary btn-sm "><i class = "fa fa-pencil"></i></a>
+                                <a href="" class= "btn btn-danger btn-sm "><i class = "fa fa-trash"></i></a>
                             </td>
                             
+                           
                             <td><?php echo $user['username'] ?></td>
                             <td><?php echo $user['name'] ?></td>
                             <td><?php echo $user['email'] ?></td>
                             <td><?php echo $user['last_login'] ?></td>
-                            <td>[profile]</td>
-                            <td>[profile]</td>
-                            <td>[elimindo]</td>
+                            
+                                
+                            <!-- admin ou outro tipo de user -->
+                            <?php if(preg_match("/admin/", $user['profile'])) : ?>
+                                <td class = "text-center"><i class = "fa fa-user" title= "Admin"></i></td>
+                            <?php else: ?>
+                                <td class = "text-center"><i class = "fa fa-user-o" title = "Not admin"></i></td>
+                            <?php endif; ?>
+                            
+                            <!-- ativo ou inativo  -->
+                            <?php if($user['active'] == 1) : ?>
+                                <td class = "text-center"><i class ="fa fa-check text-success"></i></td>
+                            <?php else : ?>
+                                <td class = "text-center"><i class = "fa fa-times text-danger"></i></td>
+                            <?php endif ; ?>
+                            
+                            <!-- eliminado ou não -->
+                            <?php if($user['deleted'] != 0) : ?>
+                                <td class = "text-center"><i class = "fa fa-check text-success"></i></td>
+                            <?php else : ?>
+                                <td class = "text-center"><i class = "fa fa-check text-danger"></i></td>
+                            <?php endif ; ?>
+    
                         </tr>
                     <?php endforeach ; ?>
                 
